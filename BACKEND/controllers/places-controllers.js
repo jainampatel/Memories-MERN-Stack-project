05f92bfad522ec91f8e043171b9a70c0e06be24b
jainampatel <jainampatel3971@gmail.com>
2021-05-27 22:51:37 -0700

    Authentication Changes
    auth-hook added, passing userId changes

--- BACKEND/controllers/places-controllers.js
+++ BACKEND/controllers/places-controllers.js
@@ -70,7 +70,7 @@ const createPlace = async (req, res, next) => {
     );
   }
 
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
 
   let coordinates;
   try {
@@ -85,12 +85,12 @@ const createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image: req.file.path,
-    creator,
+    creator: req.userData.userId,
   });
 
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch (err) {
     const error = new HttpError(
       "Creating place failed, please try again.",
@@ -179,7 +179,7 @@ const deletePlace = async (req, res, next) => {
     return next(error);
   }
 
-  if (place.creator.toString() !== req.userData.userId) {
+  if (place.creator.id !== req.userData.userId) {
     const error = new HttpError(
       "You are not allowed to delete this place.",
       401
--- BACKEND/controllers/users-controllers.js
+++ BACKEND/controllers/users-controllers.js
@@ -110,7 +110,7 @@ const login = async (req, res, next) => {
   if (!existingUser) {
     const error = new HttpError(
       "Invalid credentials, could not log you in.",
-      401
+      403
     );
     return next(error);
   }
@@ -130,7 +130,7 @@ const login = async (req, res, next) => {
   if (!isValidPassword) {
     const error = new HttpError(
       "Invalid credentials, could not log you in.",
-      401
+      403
     );
     return next(error);
   }
