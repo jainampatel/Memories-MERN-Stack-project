da0b86fb1f63725dbcb77d479b78a2b5128e5a0f
jainampatel <jainampatel3971@gmail.com>
2021-05-29 23:50:54 -0700

    Few changes

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -49,11 +49,11 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://jainam:jainam123@cluster0.renza.mongodb.net/mern?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.renza.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((err) => {
     console.log(err);
